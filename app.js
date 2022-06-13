e0a6d155cb02c07266859e43c065106cae66ca31
YenYu-Tung <74877687+YenYu-Tung@users.noreply.github.com>
2022-06-14 01:03:19 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb+srv://alpha:camp@cluster0.e6ngl.mongodb.net/todo-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+const Todo = require('./models/todo')
 
 
 const db = mongoose.connection
@@ -17,10 +18,13 @@ db.once('open', () => {
 const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
-app.get('/', (req, res) => {
-  res.render('index')
-})
 
+app.get('/', (req, res) => {
+  Todo.find()//取出Todo model的資料
+  .lean() //model object turn into JS
+  .then(todos => res.render('index', {todos}))
+  .catch(error => console.error(error))  
+})
 
 app.get('/', (req, res) => {
   res.send('lalala')
