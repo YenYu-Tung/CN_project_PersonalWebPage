0307d1fb3593063090d8e188709d089b1492fdae
YenYu-Tung <74877687+YenYu-Tung@users.noreply.github.com>
2022-06-13 21:10:32 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
 app.get('/', (req, res) => {
   res.send('lalala')
 })
