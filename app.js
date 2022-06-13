ee82e760bc2a10c2bc146906fed19345dbad8abc
YenYu-Tung <74877687+YenYu-Tung@users.noreply.github.com>
2022-06-13 20:28:41 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb+srv://alpha:camp@cluster0.e6ngl.mongodb.net/todo-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('lalala')
 })
 
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
 })
