aed34755cd039f6512d60f1dee2a45e5c41050e7
YenYu-Tung <74877687+YenYu-Tung@users.noreply.github.com>
2022-06-14 21:38:46 +0800

    feature: add detail page

--- app.js
+++ app.js
@@ -37,8 +37,12 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.get('/', (req, res) => {
-  res.send('todo-list')
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', {todo}))
+    .catch(error => console.log(error))
 })
 
 
